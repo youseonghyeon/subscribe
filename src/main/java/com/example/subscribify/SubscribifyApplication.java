f7a6ce8b16503a0acd5c38c07354c1de51e8ba63
seonghyeon <dolla_@naver.com>
2023-09-03 14:52:24 +0900

    first commit

---
+++ src/main/java/com/example/subscribify/SubscribifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.subscribify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscribifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscribifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/subscribify/SubscribifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subscribify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscribifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
